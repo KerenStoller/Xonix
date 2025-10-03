0676ceab16f7dbcc3e670fb1208bc69e8526f5ad
Keren Stoller <keren123stoller@gmail.com>
2025-10-03 21:50:46 +0300

    added some more UI

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+    public void OpenGameScene()
+    {
+        Debug.Log("Opening game scene");
+        UnityEngine.SceneManagement.SceneManager.LoadScene("GameScene");
+    }
+}
